b140d4597a16e158d37bf1773da1cc68996365f4
Jacqueline <jcasalia@yahoo.com.br>
2023-09-27 12:12:03 -0300

    paciente

---
+++ src/main/java/med/voll/api/dto/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,31 @@
+package med.voll.api.dto.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.dto.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        @NotBlank String nome,
+
+        @NotBlank
+        @Email
+        String email ,
+
+        @NotBlank String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+
+        String cpf,
+      @NotNull
+        // para validar outra anotação coloca o valid
+      @Valid
+      DadosEndereco endereco
+
+
+) {
+
+
+}
---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,43 @@
+package med.voll.api.paciente;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import med.voll.api.dto.paciente.DadosCadastroPaciente;
+import med.voll.api.endereco.Endereco;
+
+@Table(name="pacientes")
+@Entity(name="Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    private  String nome;
+    private String email;
+
+    private  String telefone;
+
+    private  String cpf;
+
+    // chamando de outra tabela
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome=dados.nome();
+        this.email= dados.email();
+        this.telefone=dados.telefone();
+        this.cpf=dados.cpf();
+        this.endereco=new Endereco(dados.endereco());
+    }
+    }
+
